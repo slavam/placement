21442120ba4a5a7f04700231a3a20e2f97720306
slavam1 <mwm1955@gmail.com>
2015-12-22 09:58:30 +0000

    After create developer MySql database

--- config/db.php
+++ config/db.php
@@ -2,11 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=127.0.0.1;dbname=soid_trudbd',
-    'dsn' => 'mysql:host=trud-bd.soid.su;dbname=soid_trudbd;',
-    'username' => 'root1',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=soid_trudbd',
+//    'dsn' => 'mysql:host=soid.su;dbname=soid_trudbd;',
+    'username' => 'slavam1',
+//    'username' => 'root1',
 //    'username' => 'user_trudbd',
-    'password' => '!Q@W#E4r5t6y',
+    'password' => '',
+//    'password' => '!Q@W#E4r5t6y',
 //    'password' => 'trudbdsql0p9o*I&U',
     'charset' => 'utf8',
     'enableSchemaCache' => true,
